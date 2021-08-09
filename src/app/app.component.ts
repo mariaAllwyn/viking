bd185374cf7961ca03cbb2ee80dc136aa6eb64c9
Allwyn <maria.allwyn@icloud.com>
2021-08-09 09:42:06 +0530

    ref update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
-import { CrudService } from '../../../viking/src/app/services/crud.service';
+import { CrudService } from '../app/services/crud.service';
 import { AlbumModel, AlbumResult } from '../app/models/album.model';
 
 @Component({
